65637a951abecfbfc34c7a467800ab490d50eefd
Regilan <mariairegilan@gmail.com>
2021-10-28 17:37:30 +0530

    Method level Exclude added StatusIndicator.class

--- src/main/java/com/britsa/commons/dto/StatusIndicator.java
+++ src/main/java/com/britsa/commons/dto/StatusIndicator.java
@@ -3,20 +3,20 @@ package com.britsa.commons.dto;
 import com.britsa.commons.loggers.CommonLogger;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.cloud.firestore.annotation.IgnoreExtraProperties;
+import com.google.cloud.firestore.annotation.Exclude;
 
 /**
  * @author Maria Irudaya Regilan J
  */
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@IgnoreExtraProperties
 public class StatusIndicator {
 
     private Boolean success;
     private ErrorResponse error;
 
     @JsonProperty("Success")
+    @Exclude
     public Boolean isSuccess() {
         return success;
     }
@@ -27,6 +27,7 @@ public class StatusIndicator {
     }
 
     @JsonProperty("Error")
+    @Exclude
     public ErrorResponse getError() {
         return error;
     }
